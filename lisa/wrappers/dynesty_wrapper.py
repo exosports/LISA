1b32e14e73905050be4a33ca798bdeeb32a6c5ca
Michael Himes <mhimes@knights.ucf.edu>
2021-07-06 17:03:41 -0400

    Updated dynesty wrapper. Fixed latex

--- lisa/wrappers/dynesty_wrapper.py
+++ lisa/wrappers/dynesty_wrapper.py
@@ -110,13 +110,13 @@ class Sampler(BaseSampler):
             ndim = np.sum(self.pstep > 0)
             if self.nchains > 1:
                 p = mp.Pool(self.nchains)
-            dy   = dynesty.DynamicNestedSampler(self.loglike, 
-                                                self.prior, 
-                                                ndim, 
-                                                bound=self.bound, 
-                                                sample=self.sample, 
-                                                queue_size=self.nchains, 
-                                                pool=p)
+            dy = dynesty.DynamicNestedSampler(self.loglike, 
+                                              self.prior, 
+                                              ndim, 
+                                              bound=self.bound, 
+                                              sample=self.sample, 
+                                              queue_size=self.nchains, 
+                                              pool=p)
 
             # Run it
             dy.run_nested(nlive_init=self.nlive, 
